055cb6b902f0358e70cd06651d671a1b2f52e7be
Glassous <glassous@example.com>
2025-11-04 23:47:26 +0800

    修复了详情页面无法显示应用logo的问题

--- src/data/shortcuts.js
+++ src/data/shortcuts.js
@@ -1,26 +1,32 @@
 // 导入各个软件的快捷键数据
-import ideaData from './shortcuts/idea.json';
-import vscodeData from './shortcuts/vscode.json';
-import chromeData from './shortcuts/chrome.json';
-import systemData from './shortcuts/system.json';
-import wordData from './shortcuts/word.json';
-import excelData from './shortcuts/excel.json';
-import powerpointData from './shortcuts/powerpoint.json';
-import edgeData from './shortcuts/edge.json';
-import firefoxData from './shortcuts/firefox.json';
-import photoshopData from './shortcuts/photoshop.json';
-import illustratorData from './shortcuts/illustrator.json';
-import premiereData from './shortcuts/premiere.json';
-import aftereffectsData from './shortcuts/aftereffects.json';
-import figmaData from './shortcuts/figma.json';
-import pycharmData from './shortcuts/pycharm.json';
-import terminalData from './shortcuts/terminal.json';
-import gitData from './shortcuts/git.json';
-import dockerData from './shortcuts/docker.json';
+import ideaData from "./shortcuts/idea.json";
+import vscodeData from "./shortcuts/vscode.json";
+import chromeData from "./shortcuts/chrome.json";
+import systemData from "./shortcuts/system.json";
+import wordData from "./shortcuts/word.json";
+import excelData from "./shortcuts/excel.json";
+import powerpointData from "./shortcuts/powerpoint.json";
+import edgeData from "./shortcuts/edge.json";
+import firefoxData from "./shortcuts/firefox.json";
+import photoshopData from "./shortcuts/photoshop.json";
+import illustratorData from "./shortcuts/illustrator.json";
+import premiereData from "./shortcuts/premiere.json";
+import aftereffectsData from "./shortcuts/aftereffects.json";
+import figmaData from "./shortcuts/figma.json";
+import pycharmData from "./shortcuts/pycharm.json";
+import terminalData from "./shortcuts/terminal.json";
+import gitData from "./shortcuts/git.json";
+import dockerData from "./shortcuts/docker.json";
 
 // 使用 import.meta.glob 动态导入所有图标
-const icons = import.meta.glob('../assets/icons/*.svg', { eager: true, as: 'url' });
-const pngIcons = import.meta.glob('../assets/icons/*.png', { eager: true, as: 'url' });
+const icons = import.meta.glob("../assets/icons/*.svg", {
+  eager: true,
+  as: "url",
+});
+const pngIcons = import.meta.glob("../assets/icons/*.png", {
+  eager: true,
+  as: "url",
+});
 Object.assign(icons, pngIcons);
 
 // 快捷键数据结构：软件 -> 系统 -> 具体快捷键
@@ -42,34 +48,40 @@ export const shortcutsData = {
   pycharm: pycharmData,
   terminal: terminalData,
   git: gitData,
-  docker: dockerData
+  docker: dockerData,
 };
 
 // 获取所有软件列表
 export const getSoftwareList = () => {
-  return Object.keys(shortcutsData).map(key => {
+  return Object.keys(shortcutsData).map((key) => {
     const software = shortcutsData[key];
     const iconPath = `../assets/icons${software.icon}`;
     return {
       id: key,
       ...software,
-      icon: icons[iconPath]
+      icon: icons[iconPath],
     };
   });
 };
 
 // 根据软件ID获取软件信息
 export const getSoftwareById = (id) => {
-  return shortcutsData[id] || null;
+  const software = shortcutsData[id];
+  if (!software) return null;
+  const iconPath = `../assets/icons${software.icon}`;
+  return {
+    ...software,
+    icon: icons[iconPath],
+  };
 };
 
 // 获取软件的系统列表
 export const getSystemsBy软件Id = (softwareId) => {
   const software = getSoftwareById(softwareId);
   if (!software) return [];
-  
-  return Object.keys(software.systems).map(key => ({
+
+  return Object.keys(software.systems).map((key) => ({
     id: key,
-    ...software.systems[key]
+    ...software.systems[key],
   }));
 };
